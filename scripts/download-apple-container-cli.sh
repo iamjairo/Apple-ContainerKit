9da532974eed5cdf26bf644e90ff2766c2c687d1
theetherGit <meenashivam9650@gmail.com>
2026-04-08 06:26:47 +0530

    CLI v0.11.0

--- scripts/download-apple-container-cli.sh
+++ scripts/download-apple-container-cli.sh
@@ -6,7 +6,7 @@ set -e
 # --- Configuration ---
 # Set the version of the Apple Container CLI you want to download.
 # You will need to update this variable for new releases.
-VERSION="0.10.0"
+VERSION="0.11.0"
 
 # Set the URL and filename for the signed installer package (If ever changes).
 PKG_URL="https://github.com/apple/container/releases/download/${VERSION}/container-${VERSION}-installer-signed.pkg"
